05028612631d599e6bc1e63b3091ec4370274f39
arminderlubana <arminder.lubana@gmail.com>
2018-08-24 15:45:37 +0530

    initial comment - api service
    an api in asp.net core 2.0 which will get request as image and will save in folder and return status with path of image to access from http url

---
+++ imageManagement/Controllers/ImageController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace imageManagement.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Image")]
+    public class ImageController : Controller
+    {
+        [HttpPost()]
+        public async Task<IActionResult> UploadImage()
+        {
+            try
+            {
+
+                if (Request.Form == null)
+                    return await Task.FromResult(BadRequest(new { isUpload = false, Error = "User has not uploaded the file." }));
+                if (Request.Form.Files.Count == 0)
+                    return await Task.FromResult(BadRequest(new { isUpload = false, Error = "User has not uploaded the file." }));
+
+                var file = Request.Form.Files[0];
+                // _logger.LogInformation("**********Upload Contract  Request***********");
+                var path = System.Environment.CurrentDirectory + "\\images\\";//ServiceUtil.GetBaseUrl(_configuration, Constant.TemplatePath) + companyId;// + "/" + file.FileName;
+                                                                              // 
+
+                //if (!Directory.Exists(path))
+                //{
+                //    Directory.CreateDirectory(path);
+                //}
+                string newfile = string.Empty;
+                string fileName = Guid.NewGuid().ToString();
+                if (file.Length > 0)
+                {
+                    newfile = fileName.ToString() + System.IO.Path.GetExtension(file.FileName);
+                    // string fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+
+                    string fullPath = Path.Combine(path, newfile);
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                }
+                return Ok(new { status = true, originalName = file.FileName, generatedName = newfile, msg = "Image upload successfull", ImageUrl = "http://localhost:65444/images/" + newfile });
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
---
+++ imageManagement/Startup.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace imageManagement
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllHeaders",
+                      builder =>
+                      {
+                          builder.AllowAnyOrigin()
+                                 .AllowAnyHeader()
+                                 .AllowAnyMethod();
+                      });
+            });
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info { Title = "Blog Service Api", Version = "V1" });
+            //});
+            services.AddMvc();
+            services.AddCors();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            // Set up custom content types - associating file extension to MIME type
+            var provider = new FileExtensionContentTypeProvider();
+            // Add new mappings
+            //provider.Mappings[".myapp"] = "application/x-msdownload";
+            //provider.Mappings[".htm3"] = "text/html";
+            provider.Mappings[".image"] = "image/png";
+            provider.Mappings[".image"] = "image/jpg";
+            // Replace an existing mapping
+            // provider.Mappings[".rtf"] = "application/x-msdownload";
+            // Remove MP4 videos.
+            provider.Mappings.Remove(".mp4");
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+            Path.Combine(Directory.GetCurrentDirectory(), "images")),
+                RequestPath = "/images",
+                ContentTypeProvider = provider
+            });
+            //app.UseDirectoryBrowser(new DirectoryBrowserOptions
+            //{
+            //    FileProvider = new PhysicalFileProvider(
+            //Path.Combine(Directory.GetCurrentDirectory(), "images")),
+            //    RequestPath = "/images"
+            //});
+            //app.UseFileServer(enableDirectoryBrowsing: true);
+            app.UseCors("AllowAllHeaders");
+            // app.UseApiKey();
+            app.UseMvc();
+        }
+    }
+}
